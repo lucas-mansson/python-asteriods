935361f8777bc84210dedff034e27788e1d69249
lucas-mansson <lu5132ma-s@student.lu.se>
2025-06-13 23:27:48 +0200

    Add player object

--- circleshape.py
+++ circleshape.py
@@ -2,7 +2,7 @@ import pygame
 
 class CircleShape(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, radius):
+    def __init__(self, x: int, y: int , radius: int):
 
         if hasattr(self, "containers"):
             super().__init__(self.containers)
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE   
+from player import Player
+
 
 def main():
     print("Starting asteroids")
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Set the screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Timer for FPS
     timer = pygame.time.Clock()
     dt = 0
 
+    # Instansiate the player
+    player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
+    
+    player.draw(screen)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         time_passed = timer.tick(60)
