648cdd65cb7996277d038183ffed622ce54c11fb
lucas-mansson <lu5132ma-s@student.lu.se>
2025-06-13 20:58:58 +0200

    added contants and draw screen

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID
 def main():
     print("Starting asteroids")
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    timer = pygame.time.Clock()
+    dt = 0
+
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill("black")
         pygame.display.flip()
 
+        time_passed = timer.tick(60)
+        dt = time_passed/1000
+        
 
 if __name__ == "__main__":
     main()
